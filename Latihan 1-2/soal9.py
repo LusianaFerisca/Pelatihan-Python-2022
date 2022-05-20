2362d2d3d72394afca7c2a5f4405e01de9acda13
LusianaFerisca <104288317+LusianaFerisca@users.noreply.github.com>
2022-05-20 15:28:10 +0700

    Create soal9.py

---
+++ Latihan 1-2/soal9.py
@@ -0,0 +1,11 @@
+Tekanan = float(input("Masukan Tekanan :"))
+print("Tekanan :",Tekanan, "Kilopascal")
+
+Tekanan_psi = Tekanan*0.145038
+print("Tekanan :",Tekanan_psi, "psi")
+
+Tekanan_mmHg= Tekanan_psi*51.7149
+print("Tekanan :",Tekanan_mmHg,"mmHg")
+
+Tekanan_atm = Tekanan*0.00986923
+print("Tekanan :" , Tekanan_atm, "atm")
