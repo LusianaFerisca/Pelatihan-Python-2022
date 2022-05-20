8d5ed30c2e5ee5b8eea2d32a2bf3170aaee10534
LusianaFerisca <104288317+LusianaFerisca@users.noreply.github.com>
2022-05-20 15:27:39 +0700

    Create soal8.py

---
+++ Latihan 1-2/soal8.py
@@ -0,0 +1,8 @@
+Tinggi = float(input("Masukan Tinggi :"))
+print("Tinggi Badan :", Tinggi, "m")
+
+Berat = float(input("Masukan Berat :"))
+print("Berat Badan :", Berat, "Kg")
+
+BMI = Berat/(Tinggi*Tinggi)
+print("Body Mass Index (BMI) adalah :", BMI,)
