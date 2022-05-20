c5ba667da653d9c9224ec4f3f6df2491fbbe2e58
LusianaFerisca <104288317+LusianaFerisca@users.noreply.github.com>
2022-05-20 15:22:08 +0700

    Create soal3.py

---
+++ Latihan 1-2/soal3.py
@@ -0,0 +1,6 @@
+# Menghitung luas persegi panjang
+Panjang = float(input("Masukan panjang :"))
+Lebar = float(input("Masukan lebar :"))
+Luas = Panjang*Lebar
+
+print("\nLuas Persegi Panjang adalah :" , Luas,"m2") 
