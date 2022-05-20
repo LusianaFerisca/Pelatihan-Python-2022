3a0810510cfe8a4001795e421e4a6e16fda85539
LusianaFerisca <104288317+LusianaFerisca@users.noreply.github.com>
2022-05-20 15:25:06 +0700

    Create Soal6.py

---
+++ Latihan 1-2/Soal6.py
@@ -0,0 +1,11 @@
+Jarak = float(input("Masukan Jarak :"))
+print("Jarak :",Jarak, "kaki")
+
+Jarak_inci = Jarak*12
+print("Jarak :",Jarak_inci, "inci")
+
+Jarak_yard= Jarak*0.3333
+print("Jarak :",Jarak_yard,"yard")
+
+Jarak_mil = Jarak*0.00018939
+print("Jarak :" , Jarak_mil, "mil")
