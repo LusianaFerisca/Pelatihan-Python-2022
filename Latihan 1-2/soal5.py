49817982c9b1c5ac426e27ed89cc3bc8e2813d93
LusianaFerisca <104288317+LusianaFerisca@users.noreply.github.com>
2022-05-20 15:24:17 +0700

    Create soal5.py

---
+++ Latihan 1-2/soal5.py
@@ -0,0 +1,8 @@
+Tinggi = float(input("Masukan Tinggi badan anda :"))
+print("Tinggi badan anda adalah :",Tinggi, "Kaki")
+
+Tinggi_Satuaninci = Tinggi*12
+print("Tinggi badan anda adalah :",Tinggi_Satuaninci, "inci")
+
+Tinggi_Satuancm = Tinggi_Satuaninci*2.54
+print("Tinggi badan anda adalah :",Tinggi_Satuancm,"cm")
