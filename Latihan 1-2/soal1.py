7563163c0d856968a4faf218943ea4ba092618de
LusianaFerisca <104288317+LusianaFerisca@users.noreply.github.com>
2022-05-20 15:13:57 +0700

    Create soal1.py

---
+++ Latihan 1-2/soal1.py
@@ -0,0 +1,14 @@
+# Biodata Jerome Polin
+Nama = "Jerome Polin Sijabat"
+TTL ="jakarta, 2 Mei 1998 "
+Jenis_kelamin = "Laki-Laki"
+Umur = 24 
+Tinggi = 175
+Pekerjaan = "Youtuber, Mahasiswa, dan Pengusaha "
+
+print("Nama \t\t\t:" ,Nama)
+print("TTL \t\t\t:" , TTL)
+print("Jenis Kelamin \t\t:" ,Jenis_kelamin)
+print("Umur \t\t\t:" ,Umur,"Tahun")
+print("Tinggi Badan \t\t:", Tinggi,"Cm")
+print("Berprofesi sebagai \t:", Pekerjaan)
